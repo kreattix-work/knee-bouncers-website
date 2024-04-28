9ea8c76ea77a581784d50ddc2a36c3f898992bc6
Prince Patel <prpatel0949@gmail.com>
2024-04-28 17:32:58 +0530

    brand

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,38 @@
 $(".menu-toggler").on("click", function () {
   $("body").toggleClass("menu-open");
 });
+
+$(".brand-items").slick({
+  slidesToShow: 8,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1,
+  speed: 3000,
+  dots: false,
+  prevArrow: null,
+  nextArrow: null,
+  cssEase: "linear",
+  waitForAnimate: true,
+  pauseOnFocus: false,
+  pauseOnHover: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 6,
+      },
+    },
+    {
+      breakpoint: 1008,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+  ],
+});
